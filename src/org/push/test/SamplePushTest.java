7f14d51e88b6afc917366c1424a2c4c7bb837a4c
VinothAnnamalai91 <vinoannamalai@yahoo.in>
2021-08-15 07:17:03 +0530

    Task Done

---
+++ src/org/push/test/SamplePushTest.java
@@ -0,0 +1,13 @@
+package org.push.test;
+
+public class SamplePushTest {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Sample Push Test");
+		
+		System.out.println("Sample Test Java");
+		
+	}
+
+}
